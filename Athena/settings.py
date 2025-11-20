92dbb0dc6b8a2a9f4039c075168b1a58d5bec548
MatiuninDaniil <Qwarry086@gmail.com>
2025-11-20 16:06:58 +0300

    Update settings.py
    подключение бд 1 шаг

--- Athena/settings.py
+++ Athena/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'Athena.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'athena_db',
+        'USER': 'postgres',
+        'PASSWORD': '6978Qwarry',  # пустой пароль
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
